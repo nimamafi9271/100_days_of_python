17f789aa29aee674e750f9c2b8962ef5c8e1f084
Nima Mafi <nima.mafi92@gmail.com>
2024-11-06 01:05:37 +1100

    finish day 6 project

---
+++ day_6/main.py
@@ -0,0 +1,17 @@
+# This is the code for day 6 project
+# It is the solution to the Maze problem in Reeborg's World playground
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
