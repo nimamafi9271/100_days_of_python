03315639c3733f9224f14ade3450eac37e276eec
Nima Mafi <nima.mafi92@gmail.com>
2024-11-12 00:07:17 +1100

    improved day 10 project

--- day_10/main.py
+++ day_10/main.py
@@ -2,19 +2,22 @@
 # This is the code to implement a simple calculator
 from art import logo
 from functions import operations
-while True:
+def calculator():
+    should_accumulate = True
     print(logo)
-    restart = False
     n1 = float(input("What's the first number?: "))
-    while not restart:
+    while should_accumulate:
         operation = input("Pick an operation: ")
         n2 = float(input("What's the next number?: "))
         result = operations[operation](n1, n2)
         print(f"{n1} {operation} {n2} = {result}")
-        should_continue = input(f"Type 'y' to continue calculating with {result}, "
+        choice = input(f"Type 'y' to continue calculating with {result}, "
                                 f"or type 'n' to start a new calculation: ").lower()
-        if should_continue == 'n':
-            restart = True
-            print('\n'*100)
-        elif should_continue == 'y':
+        if choice == 'y':
             n1 = result
+        else:
+            should_accumulate = False
+            print('\n' * 100)
+            calculator()
+calculator()
+
