40012bbfdc4b5b4926d0fcd025e328b6f737724f
Nima Mafi <nima.mafi92@gmail.com>
2024-11-12 15:21:48 +1100

    improve code for day 10 project

--- day_10/main.py
+++ day_10/main.py
@@ -3,21 +3,22 @@
 from art import logo
 from functions import operations
 def calculator():
-    should_accumulate = True
     print(logo)
-    n1 = float(input("What's the first number?: "))
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
     while should_accumulate:
+        for symbol in operations:
+            print(symbol)
         operation = input("Pick an operation: ")
-        n2 = float(input("What's the next number?: "))
-        result = operations[operation](n1, n2)
-        print(f"{n1} {operation} {n2} = {result}")
-        choice = input(f"Type 'y' to continue calculating with {result}, "
-                                f"or type 'n' to start a new calculation: ").lower()
+        num2 = float(input("What is the second number?: "))
+        result = operations[operation](num1, num2)
+        print(f"{num1} {operation} {num2} = {result}")
+        choice = input(f"Type 'y' to continue calculating with {result}, otherwise type 'n' to start a new calculation: ").lower()
         if choice == 'y':
-            n1 = result
+            num1 = result
         else:
             should_accumulate = False
-            print('\n' * 100)
+            print("\n"*100)
             calculator()
 calculator()
 
